79c03e9c78756db8032025302c0f4dc56ae82c0b
JuliaXmas <julia069910@gmail.com>
2023-12-20 18:28:26 +0300

    Added tasks from 1 to 9.3

--- hw2/script.js
+++ hw2/script.js
@@ -1,1 +1,56 @@
 let a = '10';
+a = '20';
+alert(a);
+
+const iphoneBirthday = 'Год выпуска первого Iphone - 2007';
+alert(iphoneBirthday);
+
+const fatherofJavaScript = 'Создатель Java Script - Бренданом Айком';
+alert(fatherofJavaScript);
+
+let x = 10;
+let y = 2;
+let result = x + y;
+alert(result);
+
+let result2 = x - y;
+alert(result2);
+
+let result3 = x * y;
+alert(result3);
+
+let result4 = x / y;
+alert(result4);
+
+let b = 2;
+let result5 = b ** 5;
+alert(result5);
+
+let l = 9;
+let i = 2;
+let result6 = l % i;
+alert(result6);
+
+let num = 1;
+alert(num + 5);
+alert(num - 3);
+alert(num * 7);
+alert(num / 3);
+alert(++num);
+alert(--num);
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+const user = {
+    name: 'Julia',
+    age: 24,
+    isAdmin: false
+}
+
+user.cityofResidence = 'Нaбережные Челны';
+
+user.age = 25;
+
+delete user.cityofResidence;
+
