09199b4b5a5c3dcf98db7850d586629ae8a906a8
JuliaXmas <julia069910@gmail.com>
2023-12-24 13:43:55 +0300

    Completed 5th task

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -39,5 +39,46 @@ let b = '3';
 alert(Number(a) + Number(b));
 
 //Задание 5
- let month = prompt('Введите название месяца');
- 
+let monthNumber = Number(prompt('Введите месяц в числовом значении'));
+
+switch (monthNumber) {
+    case 12:
+        alert('Зима');
+        break;
+    case 1:
+        alert('Зима');
+        break;
+    case 2:
+        alert('Зима');
+        break;
+    case 3:
+        alert('Весна');
+        break;
+    case 4:
+        alert('Весна');
+        break;
+    case 5:
+        alert('Весна');
+        break;
+    case 6:
+        alert('Лето');
+        break;
+    case 7:
+        alert('Лето');
+        break;
+    case 8:
+        alert('Лето');
+        break;
+    case 9:
+        alert('Осень');
+        break;
+    case 10:
+        alert('Осень');
+        break;
+    case 11:
+        alert('Осень');
+        break;
+    default:
+        alert('Такого месяца не существует!');
+        break;
+}
