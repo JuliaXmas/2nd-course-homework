7bfdce2f69c691e86804761b401cd30317c59f60
JuliaXmas <julia069910@gmail.com>
2023-12-30 13:34:52 +0300

    Added game

--- 2nd-course-homework/index.js
+++ 2nd-course-homework/index.js
@@ -1,4 +1,17 @@
-let a = '10';
-a = '20';
-alert(a);
+let number = Number(prompt('Введите месяц в числовом значении'));
 
+function date(number) {
+    if ((number >= 1 && number <= 2) || number === 12) {
+        return `Зима`;
+    } else if (number >= 3 && number <= 5) {
+        return `Весна`;
+    } else if (number >= 6 && number <= 8) {
+        return `Лето`;
+    } else if (number >= 9 && number <= 11) {
+        return `Осень`;
+    } else {
+        return 'Такого месяца не существует!'
+    }
+}
+
+alert(date(number));
