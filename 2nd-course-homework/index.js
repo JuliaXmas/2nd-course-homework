409e863ddbe334b6b7cb8ed06e4d49f48f396f86
JuliaXmas <julia069910@gmail.com>
2024-01-04 14:33:23 +0300

    Corrected version

--- 2nd-course-homework/index.js
+++ 2nd-course-homework/index.js
@@ -1,17 +1,17 @@
-let number = Number(prompt('Введите месяц в числовом значении'));
+function date() {
+    let number = Number(prompt('Введите месяц в числовом значении'));
 
-function date(number) {
     if ((number >= 1 && number <= 2) || number === 12) {
-        return `Зима`;
+        alert(`Зима`);
     } else if (number >= 3 && number <= 5) {
-        return `Весна`;
+        alert(`Весна`);
     } else if (number >= 6 && number <= 8) {
-        return `Лето`;
+        alert(`Лето`);
     } else if (number >= 9 && number <= 11) {
-        return `Осень`;
+        alert(`Осень`);
     } else {
-        return 'Такого месяца не существует!'
+        alert('Такого месяца не существует!')
     }
 }
 
-alert(date(number));
+
