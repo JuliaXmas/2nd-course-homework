88970acf685086b9491bc616351123b03aa8ccca
JuliaXmas <julia069910@gmail.com>
2024-01-14 17:24:27 +0300

    Completed task 3

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -11,3 +11,8 @@ for (let i = 0; i < arr.length; i++) {
 const numbs = [1, 5, 4, 10, 0, 3];
 
 console.log(numbs.indexOf(4))
+
+//Задание 3
+let numbers = [1, 3, 5, 10, 20];
+numbers = numbers.join(' ');
+console.log(numbers);
