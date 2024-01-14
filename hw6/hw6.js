4896f3ff5a0d31645f3bafc42e2b379c80b1110b
JuliaXmas <julia069910@gmail.com>
2024-01-14 17:34:11 +0300

    Completed task 4

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -16,3 +16,15 @@ console.log(numbs.indexOf(4))
 let numbers = [1, 3, 5, 10, 20];
 numbers = numbers.join(' ');
 console.log(numbers);
+
+//Задание 4 
+let table = []
+
+for (let i = 0; i < 3; i++) {
+    table[i] = [];
+    for (let j = 0; j < 3; j++) {
+        table[i][j] = 1;
+    };
+};
+
+console.log(table);
