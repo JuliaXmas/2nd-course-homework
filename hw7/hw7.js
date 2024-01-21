14ec9899067e3d7a87831cd5a16bb8d08380b976
JuliaXmas <julia069910@gmail.com>
2024-01-21 16:25:04 +0300

    Completed task 2 and corrected task 1

--- hw7/hw7.js
+++ hw7/hw7.js
@@ -1,2 +1,17 @@
 //Задание 1
-str.toUpperCase()
+let str = 'Строка';
+str = str.toUpperCase();
+console.log(str);
+
+
+//Задание 2 
+const arr = ['Бабочка', 'Пекарь', 'Бабушка', 'Бушака', 'Барин', 'Боярин', 'Князь']
+const string = 'ба';
+const arrString = [];
+arr.forEach((person) => {
+    if (person.toLowerCase().startsWith(string)) {
+        arrString.push(person)
+    }
+});
+
+console.log(arrString);
