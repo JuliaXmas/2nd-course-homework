f81659ebc4e6fa281a3cebe180f654e085f44927
JuliaXmas <julia069910@gmail.com>
2024-01-25 17:49:49 +0300

    Added (but didn't understand) task 4

--- hw8/hw8.js
+++ hw8/hw8.js
@@ -67,3 +67,17 @@ const timer = (currentDate) => {
 };
 
 timer(currentDate);
+
+//Задание 4
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
