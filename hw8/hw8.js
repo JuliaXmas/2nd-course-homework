34a1f724110c38c4e823e9d36fd2e23a7a27ca36
JuliaXmas <julia069910@gmail.com>
2024-01-25 17:08:19 +0300

    Added task 2

--- hw8/hw8.js
+++ hw8/hw8.js
@@ -16,3 +16,37 @@ people.sort(function (a, b) {
 });
 
 console.log(people);
+
+//Задание 2
+function isPositive(num) {
+    if (num > 0) {
+        return num;
+    }
+}
+
+function isMale(item) {
+    if (item.gender === 'male') {
+        return item;
+    }
+}
+function filter(array, ruleFunction) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+const people2 = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people2, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
