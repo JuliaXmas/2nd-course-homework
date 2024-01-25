1584abf727b8c21bc1a21b5a69789d3988ebb165
JuliaXmas <julia069910@gmail.com>
2024-01-25 17:51:32 +0300

    Added task 5

--- hw8/hw8.js
+++ hw8/hw8.js
@@ -81,3 +81,23 @@ function sayHi(name) {
 }
 
 delayForSecond(() => sayHi('Глеб'));
+
+//Задание 5
+// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
+// а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+delayForSecond(() => sayHi('Глеб'));
